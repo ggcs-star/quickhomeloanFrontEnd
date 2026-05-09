3c48030b5336539409f0f289f9fc8b815d14f9e1
ggcs-star <ggcs@globalgarner.com>
2026-05-09 12:06:15 +0530

    Not Found Page Add And Badges romes form bank And Facebook symbol removed

--- src/container/Footer/index.jsx
+++ src/container/Footer/index.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
               {/* Social Links */}
               <div className="flex gap-3">
                 {[
-                  { Icon: Facebook, href: "#" },
+                  // { Icon: Facebook, href: "#" },
                   {
                     Icon: Instagram,
                     href: "https://www.instagram.com/quickhomeloanoffical/",
